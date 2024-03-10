8edd9b35815c45a662dc65edb41d025bbf55da52
HBB <84764128+skinheadH@users.noreply.github.com>
2024-03-10 12:03:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const OPT = {
     "otherCodeC":``,//其他参数A
     "otherCodeD":``,//其他参数A
     "otherCodeE":``,//其他参数A
-    "copyRight" :`Powered by <a href="https://github.com/skinheadH/cloudflare-blog">skinheadH</a> & <a href="http://blog.hbb.cloudns.org/">piaochong </a>`,//页脚声明链接
+    "copyRight" :`From <a href="https://github.com/skinheadH/cloudflare-blog">skinheadH</a> & Example <a href="http://blog.hbb.cloudns.org/">piaochong </a>`,//页脚声明链接
 "robots":`User-agent: *
 Disallow: /admin`//robots.txt设置
 };
